09c57982ccdb279dfbc970a28db8cdaa884f9f50
Cotelea <marius.cotelea1003@gmail.com>
2020-10-27 19:28:12 +0200

    am adaugat tema cu cele 10 clase

---
+++ Tema 10.09.2020/Main.java
@@ -0,0 +1,65 @@
+public class Main{
+	public static void main(String[] args) {
+		cladiri A=new cladiri();
+	}
+}
+class ghiozdan{
+	public static void penar(String[] args){
+		byte pixuri=3;
+		byte creioane=2;
+		byte radiere=1;
+		byte ascutitori=1;
+	}
+	public static void notes(String[] args){
+		int carti=10;
+		int caiete=10;
+	}
+}
+class dog{
+	int picioare=4;
+	String rasa="canidae";
+}
+class cat{
+	int picioare=4;
+	String rasa="felidae";
+}
+class cutie_ochelari{
+	public static void ochelari(String[] args){
+		System.out.println("Este doar o pereche de ochelari in cutie");
+	} 
+}
+class cutie_casti{
+	int casti=2;
+}
+class calculator_desktop{
+	public static void pc(String[] args) {
+		int placa_video=1;
+		int power_supply=1;
+		int main_board=1;
+		int ram=2;
+		String ram_capacity="4GB";
+	}
+	public static void periferice(String[] args) {
+		int monitor=1;
+		int boxe=2;
+		int tastatura=1;
+		int mouse=1;
+		String imprimanta="color";
+	}
+}
+class telefon{
+	String marca="Xiaomi";
+	String sistem_de_operare="Android";
+}
+class automobil{
+	int nr_usi=4;
+	String marca="Dacia";
+}
+class cladiri{
+	public static void cladiri_noi(String[] args) {
+		System.out.println("Exista cladiri noi");
+	}
+	public static void cladiri_vechi(String[] args) {
+		System.out.println("Exista cladiri vechi");
+	}
+}
